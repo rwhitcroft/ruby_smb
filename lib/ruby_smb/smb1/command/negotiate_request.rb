fb534251da2556c834b1d435c21a3142a86a2c34
Louis Sato <lsato@rapid7.com>
2015-09-28 18:33:50 -0500

    added test for rest of attributes

--- lib/ruby_smb/smb1/command/negotiate_request.rb
+++ lib/ruby_smb/smb1/command/negotiate_request.rb
@@ -3,22 +3,79 @@ module SMB1
 module Command
 class  NegotiateRequest < RubySMB::SMB_Field::Composite_Field
 
-  def initialize(dialects:[], params:{})
+  def initialize
     super()
-
-    add_field( RubySMB::SMB_Field::SMB_Header.new (
-      protocol: "\xFFSMB",
-       command: "\x72"
-        status: "\x00"
-      .
-      .
-      mid: "\x00"))
-
 
     add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
                  f.name         = :protocol
                  f.n_bytes_spec = 4
-                 f.value        = header_params[protocol]
+                 f.value        = "\xFFSMB"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :command
+                 f.n_bytes_spec = 1
+                 f.value        = "\x72"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :status
+                 f.n_bytes_spec = 4
+                 f.value        = "\x00"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :flags
+                 f.n_bytes_spec = 1
+                 f.value        = "\x18"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :flags2
+                 f.n_bytes_spec = 2
+                 f.value        = "\x48\x01"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :pid_high
+                 f.n_bytes_spec = 2
+                 f.value        = "\x00"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :security_features
+                 f.n_bytes_spec = 8
+                 f.value        = "\x00"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :reserved
+                 f.n_bytes_spec = 2
+                 f.value        = "\x00"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :tid
+                 f.n_bytes_spec = 2
+                 f.value        = "\xFF\xFF"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :pid_low
+                 f.n_bytes_spec = 2
+                 f.value        = "\x00"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :uid
+                 f.n_bytes_spec = 2
+                 f.value        = "\x00"
+               end )
+
+    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+                 f.name         = :mid
+                 f.n_bytes_spec = 2
+                 f.value        = "\x00"
                end )
 
     add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
@@ -29,22 +86,22 @@ class  NegotiateRequest < RubySMB::SMB_Field::Composite_Field
 
 
 
-    add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
-                 f.name         = :dialects
-                 f.n_bytes_spec = 34
-                 f.dialects = RubySMB::SMB_Field::Composite_Field.new do |d|
-                     d.add_field(RubySMB::SMB_Field::Leaf_Field.new do |df|
-                      df.name   = :d1
-                      df.n_bytes_spec = 12
-                      df.value  = "\x02NT LM 0.12\x00"
-                     end)
-
-                     d.add_field()
-
-
-
-                 f.value        = "\x02NT LM 0.12\x00\x02SMB 2.002\x00\x02SMB 2.???\x00"
-               end )
+    # add_field( RubySMB::SMB_Field::Leaf_Field.new do |f|
+    #              f.name         = :dialects
+    #              f.n_bytes_spec = 34
+    #              f.dialects = RubySMB::SMB_Field::Composite_Field.new do |d|
+    #                  d.add_field(RubySMB::SMB_Field::Leaf_Field.new do |df|
+    #                   df.name   = :d1
+    #                   df.n_bytes_spec = 12
+    #                   df.value  = "\x02NT LM 0.12\x00"
+    #                  end)
+    #
+    #                  d.add_field()
+    #
+    #
+    #
+    #              f.value        = "\x02NT LM 0.12\x00\x02SMB 2.002\x00\x02SMB 2.???\x00"
+    #            end )
 
 
   end
--- spec/lib/ruby_smb/smb1/command/negotiate_request_spec.rb
+++ spec/lib/ruby_smb/smb1/command/negotiate_request_spec.rb
@@ -23,6 +23,181 @@ RSpec.describe NegotiateRequest do
       end
     end
 
+    describe '#field(:command)' do
+      let(:field) { command.field(:command) }
+
+      it 'has name :command' do
+        expect(field.name).to eql :command
+      end
+
+      it 'has n_bytes_spec = 1' do
+        expect(field.n_bytes_spec).to eql 1
+      end
+
+      it 'has default value = "\x72"' do
+        expect(field.value).to eql "\x72"
+      end
+    end
+
+    describe '#field(:status)' do
+      let(:field) { command.field(:status) }
+
+      it 'has name :status' do
+        expect(field.name).to eql :status
+      end
+
+      it 'has n_bytes_spec = 4' do
+        expect(field.n_bytes_spec).to eql 4
+      end
+
+      it 'has default value = "\x00"' do
+        expect(field.value).to eql "\x00"
+      end
+    end
+
+    describe '#field(:flags)' do
+      let(:field) { command.field(:flags) }
+
+      it 'has name :flags' do
+        expect(field.name).to eql :flags
+      end
+
+      it 'has n_bytes_spec = 1' do
+        expect(field.n_bytes_spec).to eql 1
+      end
+
+      it 'has default value = "\x18"' do
+        expect(field.value).to eql "\x18"
+      end
+    end
+
+    describe '#field(:flags2)' do
+      let(:field) { command.field(:flags2) }
+
+      it 'has name :flags2' do
+        expect(field.name).to eql :flags2
+      end
+
+      it 'has n_bytes_spec = 2' do
+        expect(field.n_bytes_spec).to eql 2
+      end
+
+      it 'has default value = "\x48\x01"' do
+        expect(field.value).to eql "\x48\x01"
+      end
+    end
+
+    describe '#field(:pid_high)' do
+      let(:field) { command.field(:pid_high) }
+
+      it 'has name :pid_high' do
+        expect(field.name).to eql :pid_high
+      end
+
+      it 'has n_bytes_spec = 2' do
+        expect(field.n_bytes_spec).to eql 2
+      end
+
+      it 'has default value = "\x00"' do
+        expect(field.value).to eql "\x00"
+      end
+    end
+
+    describe '#field(:security_features)' do
+      let(:field) { command.field(:security_features) }
+
+      it 'has name :security_features' do
+        expect(field.name).to eql :security_features
+      end
+
+      it 'has n_bytes_spec = 8' do
+        expect(field.n_bytes_spec).to eql 8
+      end
+
+      it 'has default value = "\x00"' do
+        expect(field.value).to eql "\x00"
+      end
+    end
+
+    describe '#field(:reserved)' do
+      let(:field) { command.field(:reserved) }
+
+      it 'has name :reserved' do
+        expect(field.name).to eql :reserved
+      end
+
+      it 'has n_bytes_spec = 2' do
+        expect(field.n_bytes_spec).to eql 2
+      end
+
+      it 'has default value = "\x00"' do
+        expect(field.value).to eql "\x00"
+      end
+    end
+
+    describe '#field(:tid)' do
+      let(:field) { command.field(:tid) }
+
+      it 'has name :tid' do
+        expect(field.name).to eql :tid
+      end
+
+      it 'has n_bytes_spec = 2' do
+        expect(field.n_bytes_spec).to eql 2
+      end
+
+      it 'has default value = "\xFF\xFF"' do
+        expect(field.value).to eql "\xFF\xFF"
+      end
+    end
+
+    describe '#field(:pid_low)' do
+      let(:field) { command.field(:pid_low) }
+
+      it 'has name :pid_low' do
+        expect(field.name).to eql :pid_low
+      end
+
+      it 'has n_bytes_spec = 2' do
+        expect(field.n_bytes_spec).to eql 2
+      end
+
+      it 'has default value = "\x00"' do
+        expect(field.value).to eql "\x00"
+      end
+    end
+
+    describe '#field(:uid)' do
+      let(:field) { command.field(:uid) }
+
+      it 'has name :uid' do
+        expect(field.name).to eql :uid
+      end
+
+      it 'has n_bytes_spec = 2' do
+        expect(field.n_bytes_spec).to eql 2
+      end
+
+      it 'has default value = "\x00"' do
+        expect(field.value).to eql "\x00"
+      end
+    end
+
+    describe '#field(:mid)' do
+      let(:field) { command.field(:mid) }
+
+      it 'has name :mid' do
+        expect(field.name).to eql :mid
+      end
+
+      it 'has n_bytes_spec = 2' do
+        expect(field.n_bytes_spec).to eql 2
+      end
+
+      it 'has default value = "\x00"' do
+        expect(field.value).to eql "\x00"
+      end
+    end
 
     describe '#field(:dialects)' do
       let(:field) { command.field(:dialects) }
